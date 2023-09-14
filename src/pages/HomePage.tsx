c09ecc8a1b49b3fa7cae3e2bf000e4f86f405894
elsiezhu <elsie.zhu2003@gmail.com>
2023-09-14 00:26:38 -0400

    add another picture:

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,23 +5,31 @@ import { ConstructionIcon } from '../icons/Icons';
 export default function HomePage(): JSX.Element {
     
     return (
-        <Box pad="large" background={"background-front"} height="large" responsive>
+        <PageContent 
+            background={"background-front"} 
+            direction="column" 
+            pad={{top: 'none'}}
+            width={{min: "50px", max: "100%"}}
+            responsive
+        >
+        <Box margin={{left: "xlarge", right: "xlarge"}} background={"background-front"} responsive>
             <Page kind="wide" gap="small" responsive>
                 <PageContent background={"background-front"} direction="column" gap="none" responsive>
-                    <Heading level={1} size="medium" alignSelf='center' textAlign="center" margin="small">
-                        this page is under construction
+                    <Box height="medium" width="medium" alignSelf="center" alignContent='center'>
+                        <Image
+                            fit="contain"
+                            src="https://elsiezhu.github.io/me_2.PNG"
+                        />
+                    </Box>
+                    <Heading level={1} size="large" alignSelf='center' textAlign="center" margin="small">
+                        Elsie Zhu
                     </Heading>
                     <Text size="large" alignSelf='center' textAlign="center">
                         check back soon!
                     </Text>
-                    <Box height="small" width="small">
-                        <Image
-                            fit="cover"
-                            src="https://elsiezhu.github.io/me_1.PNG"
-                        />
-                    </Box>
                 </PageContent>
             </Page>
         </Box>
+        </PageContent>
     );
 }
